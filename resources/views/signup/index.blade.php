4a34c6a1bed150b5efe94f2538fe38c97ae71aa0
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-09-03 01:32:28 +0900

    add-errors

--- resources/views/signup/index.blade.php
+++ resources/views/signup/index.blade.php
@@ -4,13 +4,34 @@
 </header>
 
 <p>{{$msg}}</p>
+@if (count($errors) > 0)
+<p>入力に問題があります。</p>
+@endif
+
 <table>
 <form action="/signup" method="post">
     {{ csrf_field() }}
-    <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-    <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
-    <tr><th>Password: </th><td><input type="password" name="password"></td></tr>
-    <tr><th>ConfirmPassword: </th><td><input type="password" name="confirmpassword"></td></tr>
+
+    @if ($errors->has('name'))
+    <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+    @endif
+      <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+
+    @if ($errors->has('mail'))
+    <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+    @endif
+      <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+
+    @if ($errors->has('password'))
+    <tr><th>ERROR</th><td>{{$errors->first('password')}}</td></tr>
+    @endif
+    <tr><th>Password: </th><td><input type="password" name="password" value="{{old('password')}}"></td></tr>
+
+    @if ($errors->has('confirmpassword'))
+    <tr><th>ERROR</th><td>{{$errors->first('confirmpassword')}}</td></tr>
+    @endif
+    <tr><th>ConfirmPassword:</th><td><input type="password" name="confirmpassword"></td></tr>
+
     <tr><th></th><td><input type="submit" value="send"></td></tr>
 </form>
 </table>
