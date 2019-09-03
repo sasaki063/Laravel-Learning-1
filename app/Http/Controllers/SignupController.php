7386ae0ed6722b74f9c968e6dde3f17bdc2d3808
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-09-03 19:56:59 +0900

    add form request SignupRequest

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -3,24 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\SignupRequest;
 
 class SignupController extends Controller
 {
-  public function index(Request $request)
+  public function index(SignupRequest $request)
   {
     return view('signup.index', ['msg'=>'フォームを入力：']);
   }
 
-  public function post(Request $request)
+  public function post(SignupRequest $request)
   {
-    $validate_rule = [
-        'name' => 'required',
-        'mail' => 'email',
-        'password' => 'required|min:8',
-        'password_confirm' => 'required|same:password',
-    ];
-    $this->validate($request, $validate_rule);
-
     return view('pages.home', ['name'=> $request->name]);
   }
 }
